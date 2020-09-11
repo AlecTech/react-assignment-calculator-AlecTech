96ddc0e5b1d113cbb05b9d4695fddb214004d644
AlecTech <eremeevoleg@gmail.com>
2020-09-11 16:19:18 -0600

    Moved all html from index.js to Calculator.js and left only one component inside ReactStrinct to control output

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,34 +3,55 @@ import React from 'react';
 // We can try a fat arrow function as well (let's try to be consistent in the real world though!)
 // If the component accepts a parameter, this is referred to as a "prop."
 // Props allow us to pass values INTO our components from a parent document / component.
-const Calculator = props => {
+function Calculator (props)
+{ // Every component should return JSX.
+ 
+  let heading = props.heading;
+  let input1 = props.input1;
+  let input2 = props.input2;
+  //let empty = props.empty;
+ 
   return (
-    // <p>
-    //   Hello&nbsp;
-    //   { props.name },
-    //   welcome to TECHCareers and its React unit!
-    //   Their favourite unit is:&nbsp;
-    //   { props.unit }
-    // </p>
     <div>
-      <h1>Welcome to my Calculator!</h1>
-      <h2>Input 1:</h2>
+      <h1> { heading } </h1>
+      <h2> { input1 } </h2>
       <input type='text' class='field' />
       <h2>Operator</h2>
       <select class='where' >
-        <option></option>
-        <option value='add'>&nbsp;+</option>
-        <option value='subtract'>&nbsp;  -  </option>
-        <option value='divid'>&nbsp;  /  </option>
-        <option value='multiply'>&nbsp; *  </option>
+         <option></option>
+         <option value='add'>+</option>
+         <option value='subtract'>-</option>
+         <option value='divid'>/</option>
+         <option value='multiply'>*</option>
       </select>
-      <h2>Input 2:</h2>
+      <h2> {input2}</h2>
       <input type='text' class='field' />
       <h2></h2>
       <input type='submit' class='submitBtn success' value='Submit'/>
-  </div>
-      
+    </div>
   );
 }
+// const Calculator = props => {
+//   return (
+//     <div>
+//       <h1>Welcome to my Calculator!</h1>
+//       <h2>Input 1:</h2>
+//       <input type='text' class='field' />
+//       <h2>Operator</h2>
+//       <select class='where' >
+//         <option></option>
+//         <option value='add'>+</option>
+//         <option value='subtract'>-</option>
+//         <option value='divid'>/</option>
+//         <option value='multiply'>*</option>
+//       </select>
+//       <h2>Input 2:</h2>
+//       <input type='text' class='field' />
+//       <h2></h2>
+//       <input type='submit' class='submitBtn success' value='Submit'/>
+//   </div>
+      
+//   );
+// }
 
 export default Calculator 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './components/Hello'; // If we want to use a component in this file, we have to import it!
-// import Calculator from './components/Calculator';
+// import Hello from './components/Hello'; // If we want to use a component in this file, we have to import it!
+import Calculator from './components/Calculator';
 import './calculator.css';
 
 // import Button1 from './components/Button';
@@ -10,20 +10,22 @@ import './calculator.css';
 ReactDOM.render(
   <React.StrictMode>
 
-    <Hello  heading="Welcome to my Calculator!"/>
-    <Hello  input="Input 1:"/> 
+
+    <Calculator heading="Welcome to my Calculator!" input1="Input 1:" input2="Input 2:"/>
+    {/* <Calculator  heading="Welcome to my Calculator!"/>
+    <Calculator  input="Input 1:"/> 
     <input type='text' class='field' />
-    <Hello input="Operators"/>
+    <Calculator input="Operators"/>
     <select class='where' >
       <option value='add'>+</option>
       <option value='subtract'>-</option>
       <option value='divid'>/</option>
       <option value='multiply'>*</option>
     </select>
-    <Hello  input="Input 2:"/>
+    <Calculator  input="Input 2:"/>
     <input type='text' class='field' />
-    <Hello empty="" />
-    <input type='submit' class='submitBtn success' value='Submit'/>
+    <Calculator empty="" />
+    <input type='submit' class='submitBtn success' value='Submit'/> */}
    
   </React.StrictMode>,
   document.getElementById('root')
