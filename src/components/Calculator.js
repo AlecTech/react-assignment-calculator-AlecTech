af41cd607021ad2b29ffe2ffdb7bc0b13ad55b05
AlecTech <eremeevoleg@gmail.com>
2020-09-11 20:08:47 -0600

    Moved input fields and submit button inside form tags and added onFormSubmit function with console logs just for test

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,14 +5,19 @@ import React, { useState } from 'react';
 // Props allow us to pass values INTO our components from a parent document / component.
 function Calculator (props)
 { // Every component should return JSX.
-  const [new1Num, setNew1Num] = useState('0');
-  const [new2Num, setNew2Num] = useState('0');
+  const [new1Num, setNew1Num] = useState(0);
+  const [new2Num, setNew2Num] = useState(0);
+  const [newOper, setNewOper] = useState('add');
 
-
-
-
-
-
+  
+  // setNew1Num = value => {
+  //   setInputs({ ...inputs, value1: parseFloat(value) });
+  // };
+  const onFormSubmit = event => {
+    event.preventDefault();
+    console.log('form submitted');
+    console.log(newOper);
+  }
 
   let heading = props.heading;
   let input1 = props.input1;
@@ -20,35 +25,49 @@ function Calculator (props)
   let oper = props.oper;
   let result = props.result;
   //let empty = props.empty;
- 
+
+  
+  
+  // var oper = form.querySelector('.oper')
   return (
     <div>
       <h1> { heading } </h1>
       <h2> { input1 } </h2>
+      
+      <h2>Operator</h2>
+      <form onSubmit = {onFormSubmit}>
       <input 
       type="number" min="0" step="1" 
       onChange={e => { setNew1Num( e.target.value ) }}
       value={new1Num}
-      class='field' />
-      <h2>Operator</h2>
-      <select class='operator' >
-         <option></option>
-         <option value='add'>+</option>
-         <option value='subtract'>-</option>
-         <option value='divide'>/</option>
-         <option value='multiply'>*</option>
-      </select>
-      <h2> {input2}</h2>
-      <input 
+      className='field' />
+        <select 
+        // name="version" onChange={handleChange} value={state.version}
+        //2nd try = value={this.state.value} onChange={this.handleChange}
+        onChange={e => { setNewOper( e.target.value ) }}
+        value={newOper}
+        className='oper' >
+          
+          <option value='add'>+</option>
+          <option value='subtract'>-</option>
+          <option value='divide'>/</option>
+          <option value='multiply'>*</option>
+        </select>
+        <input 
       type="number" min="0" step="1" 
       onChange={e => { setNew2Num( e.target.value ) }}
       value={new2Num}
-      class='field' />
+      className='field' />
+      <input type='submit' className='submitBtn success' value='Submit'/>
+      </form>
+      <h2> {input2}</h2>
+      
       <h2></h2>
-      <input type='submit' class='submitBtn success' value='Submit'/>
-      <h3>RESULT: {new1Num} + {new2Num} {result} </h3>
+      
+      <h3>RESULT: {new1Num} {newOper} {new2Num} {result} </h3>
     </div>
   );
+  
 }
 export default Calculator
 
